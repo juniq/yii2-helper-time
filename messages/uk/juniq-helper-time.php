b8cdd0593ad3c1b3664f3b768a711777bb01e790
Alex <developer-av@yandex.ru>
2017-04-17 16:26:05 +0300

    Was added Russian and Ukrainian localization

---
+++ messages/uk/juniq-helper-time.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    '{n, plural, one{1 ds} other{# ds}}' => '{n, plural, one{# день} few{# дні} many{# днів} other{# днів}}',
+    '{n, plural, one{1 hs} other{# hs}}' => '{n, plural, one{# година} few{# години} many{# годин} other{# годин}}',
+    '{n, plural, one{1 is} other{# is}}' => '{n, plural, one{# хвилина} few{# хвилини} many{# хвилин} other{# хвилин}}',
+    '{n, plural, one{1 ms} other{# ms}}' => '{n, plural, one{# місяць} few{# місяці} many{# місяців} other{# місяців}}',
+    '{n, plural, one{1 ss} other{# ss}}' => '{n, plural, one{# секунда} few{# секунди} many{# секунд} other{# секунд}}',
+    '{n, plural, one{1 ws} other{# ws}}' => '{n, plural, one{# тиждень} few{# тижні} many{# тижнів} other{# тижнів}}',
+    '{n, plural, one{1 ys} other{# ys}}' => '{n, plural, one{# рік} few{# роки} many{# років} other{# років}}',
+
+    '{n, plural, one{1 d} other{# d}}' => '{n, plural, one{1 д} other{# д}}',
+    '{n, plural, one{1 h} other{# h}}' => '{n, plural, one{1 г} other{# г}}',
+    '{n, plural, one{1 i} other{# i}}' => '{n, plural, one{1 хв} other{# хв}}',
+    '{n, plural, one{1 m} other{# m}}' => '{n, plural, one{1 міс} other{# міс}}',
+    '{n, plural, one{1 s} other{# s}}' => '{n, plural, one{1 сек} other{# сек}}',
+    '{n, plural, one{1 w} other{# w}}' => '{n, plural, one{1 тиж} other{# тиж}}',
+    '{n, plural, one{1 y} other{# y}}' => '{n, plural, one{1 р} other{# р}}',
+];
